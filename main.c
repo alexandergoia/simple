7e22729ec21c4f1756a4b0c93754698cf854f5eb
Alexander Goia <67803036+alexandergoia@users.noreply.github.com>
2023-10-31 08:57:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,15 @@
 #include <stdio.h>
 
 int *addr;
-
 void do_stuff();
 
 int main(int argc, char *argv[])
 {
     addr = & argc;
-   
-    int i;
-    for(i = 0; i < argc; i++)
-        printf("%s ", argv[i]);
-    printf("\n");
-
     (*addr)--;
-
     do_stuff();
 
     return (argc > 0) ? main(argc, argv) : 0;
 }
 
-void do_stuff()
-{
-    printf("doing something...\n");
-}
+void do_stuff() {}
